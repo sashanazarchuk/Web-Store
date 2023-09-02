a311b9fbc4862da3592be62ed50a710f4d2b64ed
Oleksander Nazarchuk <ss.nazarchuk2@gmail.com>
2023-09-02 19:54:53 +0300

    Configured and Added ProductService

---
+++ Server/BusinessLogic/DTOs/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.DTOs
+{
+    public class ProductDto
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Server/BusinessLogic/Services/ProductService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using BusinessLogic.DTOs;
+using BusinessLogic.Interfaces;
+using Entities.Data;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly AppDbContext context;
+        private readonly IMapper mapper;
+
+        public ProductService(AppDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+
+        //A method to get a list of products
+        public async Task<IEnumerable<ProductDto>> GetAllProduct()
+        {
+            // Got a list of products from the database
+            var result = await context.Products.ToListAsync();
+
+            //returning mapped product list
+            return mapper.Map<IEnumerable<ProductDto>>(result);
+        }
+    }
+}
