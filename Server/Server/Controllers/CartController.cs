64ecd7f52eaf071697ac8d48e54c4028e8a4d890
Oleksander Nazarchuk <ss.nazarchuk2@gmail.com>
2023-09-03 16:36:54 +0300

    Configured shopping cart methods

---
+++ Server/Server/Controllers/CartController.cs
@@ -0,0 +1,114 @@
+﻿using BusinessLogic.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartService service;
+
+        public CartController(ICartService service)
+        {
+            this.service = service;
+        }
+
+
+        [HttpPost("AddToCart{id}")]
+        public IActionResult AddToCart([FromRoute] int id)
+        {
+            try
+            {
+                service.AddToCart(id);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                string errorMessage = "Error adding product to cart";
+                return BadRequest(errorMessage);
+            }
+        }
+
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                return Ok(service.GetCartItems());
+
+            }
+            catch (Exception)
+            {
+                string errorMessage = "Error retrieving product list";
+                return BadRequest(errorMessage);
+            }
+        }
+
+
+        [HttpDelete("Delete{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                service.RemoveItem(id);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                string errorMessage = "Error during remove item from the cart";
+                return BadRequest(errorMessage);
+            }
+        }
+
+
+        [HttpDelete("EmptyCart")]
+        public IActionResult EmptyCart()
+        {
+            try
+            {
+                service.ClearCart();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                string errorMessage = "Error during clear cart";
+                return BadRequest(errorMessage);
+            }
+        }
+
+
+
+        [HttpGet("GetCartId")]
+        public IActionResult GetCartId()
+        {
+            try
+            {
+                return Ok(service.GetCartId());
+            }
+            catch (Exception)
+            {
+                string errorMessage = "Error during show CartId";
+                return BadRequest(errorMessage);
+            }
+        }
+
+
+
+        [HttpGet("GetTotal")]
+        public IActionResult GetTotal()
+        {
+            try
+            {
+                return Ok(service.GetTotal());
+            }
+            catch (Exception)
+            {
+                string errorMessage = "Error during show total price products in the cart";
+                return BadRequest(errorMessage);
+            }
+        }
+    }
+}
---
+++ Server/Server/Services/CartService.cs
@@ -0,0 +1,114 @@
+﻿using BusinessLogic.Interfaces;
+using Entities.Data;
+using Entities.Models;
+using System.Text;
+
+
+namespace BusinessLogic.Services
+{
+    public class CartService : ICartService
+    {
+        private readonly AppDbContext context;
+        private readonly IHttpContextAccessor httpContext;
+
+
+        public const string CartSessionKey = "CartId";
+        public string ShoppingCartId { get; set; }
+
+        public CartService(AppDbContext context, IHttpContextAccessor httpContext)
+        {
+            this.context = context;
+            this.httpContext = httpContext;
+        }
+
+        //Method add product to cart 
+        public void AddToCart(int id)
+        {
+            ShoppingCartId = GetCartId();
+
+            var cartItem = context.CartItem.SingleOrDefault(c => c.CartId == ShoppingCartId && c.ProductId == id);
+            if (cartItem == null)
+            {
+                cartItem = new CartItem
+                {
+                    ItemId = Guid.NewGuid().ToString(),
+                    ProductId = id,
+                    CartId = ShoppingCartId,
+                    Product = context.Products.SingleOrDefault(
+                    p => p.ProductID == id),
+                    Quantity = 1
+                };
+                context.CartItem.Add(cartItem);
+            }
+            else
+            {
+                cartItem.Quantity++;
+            }
+            context.SaveChanges();
+        }
+
+        //Method to get the unique ID of the user's shopping cart
+        public string GetCartId()
+        {
+            if (httpContext.HttpContext.Session.GetString(CartSessionKey) == null)
+            {
+                if (!string.IsNullOrWhiteSpace(httpContext.HttpContext.User.Identity.Name))
+                {
+                    httpContext.HttpContext.Session.SetString(CartSessionKey, httpContext.HttpContext.User.Identity.Name);
+                }
+                else
+                {
+                    Guid tempCartId = Guid.NewGuid();
+                    httpContext.HttpContext.Session.SetString(CartSessionKey, tempCartId.ToString());
+                }
+            }
+            return httpContext.HttpContext.Session.GetString(CartSessionKey);
+        }
+
+        //Method to show cart product list
+        public IEnumerable<CartItem> GetCartItems()
+        {
+            ShoppingCartId = GetCartId();
+
+            return context.CartItem.Where(c => c.CartId == ShoppingCartId).ToList();
+        }
+
+        //Method to delete current product from cart
+        public void RemoveItem(int productId)
+        {
+            ShoppingCartId = GetCartId();
+
+            var cartItem = context.CartItem.SingleOrDefault(c => c.CartId == ShoppingCartId && c.ProductId == productId);
+
+            if (cartItem != null)
+            {
+                context.CartItem.Remove(cartItem);
+                context.SaveChanges();
+            }
+        }
+
+        //Method delete all product from the cart
+        public void ClearCart()
+        {
+            ShoppingCartId = GetCartId();
+
+            var cartItems = context.CartItem
+                .Where(cart => cart.CartId == ShoppingCartId);
+
+            context.CartItem.RemoveRange(cartItems);
+            context.SaveChanges();
+        }
+
+        // Method to get total amount of products in the cart
+        public decimal GetTotal()
+        {
+            ShoppingCartId = GetCartId();
+
+            decimal total = context.CartItem
+                .Where(cart => cart.CartId == ShoppingCartId)
+                .Sum(cart => cart.Quantity * cart.Product.Price);
+
+            return total;
+        }
+    }
+}
